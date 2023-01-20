d1035d38669257977bd440a469c2189e37742f5f
Mike Donovan <mike@mikedonovan.dev>
2023-01-19 16:05:01 -0800

    Added pig latin functionality
    Co-authored-by: Michael Sol <sol.mike.j@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-window.onload = () => {
-  document.querySelector('button').onclick = () => {
-    let userInput = parseInt(document.querySelector('input').value)
-    for (let i=0; i<=userInput; i++) {
-      let binaryResult = reversedBinary(i);
-      document.querySelector('.column:first-of-type').innerHTML += `<div>${i}</div>`;
-      document.querySelector('.column:last-of-type').innerHTML += `<div>${binaryResult.join('')}</div>`;
-    }
-  }
-}
+// window.onload = () => {
+//   document.querySelector('button').onclick = () => {
+//     let userInput = parseInt(document.querySelector('input').value)
+//     for (let i=0; i<=userInput; i++) {
+//       let binaryResult = reversedBinary(i);
+//       document.querySelector('.column:first-of-type').innerHTML += `<div>${i}</div>`;
+//       document.querySelector('.column:last-of-type').innerHTML += `<div>${binaryResult.join('')}</div>`;
+//     }
+//   }
+// }
 
 function translateWord(inputString){
   let outputString = '';
@@ -49,46 +49,46 @@ function getConsecutiveConsonants(word) {
   return consonants;
 }
 
-function takeInBinary(inputDecimalNumber){
-  let returnBinaryArray = ['0'];
-  for (let i = 0; i<inputDecimalNumber; i++){
-    if(returnBinaryArray[returnBinaryArray.length - 1] == '0'){
-      returnBinaryArray[returnBinaryArray.length - 1] = '1';
-    }else{
-      returnBinaryArray[returnBinaryArray.length - 1] = '1';
-      let reversedDigitArray = returnBinaryArray.reverse();
-      for (let k=0; k<reversedDigitArray.length; k++) {
-        let currentDigit = reversedDigitArray[k];
-        if (currentDigit === '0') {
-          // change to one and end
-          reversedDigitArray[k] = '1'
-          break;
-        } else {
+// function takeInBinary(inputDecimalNumber){
+//   let returnBinaryArray = ['0'];
+//   for (let i = 0; i<inputDecimalNumber; i++){
+//     if(returnBinaryArray[returnBinaryArray.length - 1] == '0'){
+//       returnBinaryArray[returnBinaryArray.length - 1] = '1';
+//     }else{
+//       returnBinaryArray[returnBinaryArray.length - 1] = '1';
+//       let reversedDigitArray = returnBinaryArray.reverse();
+//       for (let k=0; k<reversedDigitArray.length; k++) {
+//         let currentDigit = reversedDigitArray[k];
+//         if (currentDigit === '0') {
+//           // change to one and end
+//           reversedDigitArray[k] = '1'
+//           break;
+//         } else {
 
-        }
-        // 
-      }
-    }
-  }
-  return returnBinaryArray.join('');
-}
+//         }
+//         // 
+//       }
+//     }
+//   }
+//   return returnBinaryArray.join('');
+// }
 
-function reversedBinary (inputNumber){
-  let returnBinary = ['0']
-  for (let i = 0; i <= inputNumber-1; i++){
-    let found = false;
-    for(let j = 0; j <= returnBinary.length -1; j++){
-      if(returnBinary[j] == '0'){
-        returnBinary[j] = '1';  //done with searching
-        found = true;
-        break;
-      }else{
-        returnBinary[j] = '0';
-      }
-    }
-    if (!found){
-    returnBinary.push('1')
-    }
-  }
-  return returnBinary.reverse();
-}
+// function reversedBinary (inputNumber){
+//   let returnBinary = ['0']
+//   for (let i = 0; i <= inputNumber-1; i++){
+//     let found = false;
+//     for(let j = 0; j <= returnBinary.length -1; j++){
+//       if(returnBinary[j] == '0'){
+//         returnBinary[j] = '1';  //done with searching
+//         found = true;
+//         break;
+//       }else{
+//         returnBinary[j] = '0';
+//       }
+//     }
+//     if (!found){
+//     returnBinary.push('1')
+//     }
+//   }
+//   return returnBinary.reverse();
+// }
